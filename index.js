b0ad70dc12a015ba3788aa484a991506ba4f0531
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2025-01-07 00:07:05 +0500

    update

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors());
 app.use(cors("*"));
-app.use(
-  cors({
-    origin:[ "http://localhost:5173",],
-  })
-);
 
 mongoose
   .connect(process.env.MONGODBURI)
